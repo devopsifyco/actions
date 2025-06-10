9a9712f6f76d5f93b80e3942362fba180f6f4536
Huy <huyntt@gmail.com>
2025-06-10 14:51:26 +0700

    update checkctl

--- check/download-check.ps1
+++ check/download-check.ps1
@@ -1,9 +1,9 @@
 # If check.exe already exists, skip download
-if (Test-Path "check.exe") {
+if (Test-Path "checkctl.exe") {
   Write-Host "check binary already exists, skipping download."
   exit 0
 }
-$asset = "check.exe"
+$asset = "checkctl.exe"
 $version = $args[0]
 if ($version) {
   $releaseUrl = "https://api.github.com/repos/devopsifyco/check-cli/releases/tags/$version"
@@ -16,4 +16,4 @@ if (-not $downloadUrl) {
   Write-Error "Could not find download URL for $asset"
   exit 1
 }
-Invoke-WebRequest -Uri $downloadUrl -OutFile "check.exe" 
+Invoke-WebRequest -Uri $downloadUrl -OutFile "checkctl.exe" 
